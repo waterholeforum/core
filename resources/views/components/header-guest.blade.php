ce3979cfbe3be8a2fa96b9aaa9772df9b8b70ff1
Toby Zerner <toby.zerner@gmail.com>
2023-02-07 20:21:18 +1030

    classes

--- resources/views/components/header-guest.blade.php
+++ resources/views/components/header-guest.blade.php
@@ -1,13 +1,13 @@
 <div class="row">
     <a
         href="{{ route('waterhole.login') }}"
-        class="btn btn--transparent btn--narrow color-accent"
+        class="header-login btn btn--transparent btn--narrow color-accent"
     >{{ __('waterhole::header.log-in') }}</a>
 
     @if (Route::has('waterhole.register'))
         <a
             href="{{ route('waterhole.register') }}"
-            class="btn btn--transparent btn--narrow color-accent"
+            class="header-register btn btn--transparent btn--narrow color-accent"
         >{{ __('waterhole::header.register') }}</a>
     @endif
 </div>
