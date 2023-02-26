92a128ea33b705652d2e7bcb1e8c2871ff5ad646
Toby Zerner <toby.zerner@gmail.com>
2023-02-26 20:31:58 +1030

    fix: improve require login check

--- src/Http/Middleware/MaybeRequireLogin.php
+++ src/Http/Middleware/MaybeRequireLogin.php
@@ -5,7 +5,10 @@ namespace Waterhole\Http\Middleware;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Waterhole\Models\Channel;
+use Waterhole\Models\Page;
 use Waterhole\Models\PermissionCollection;
+use Waterhole\Models\StructureLink;
 
 /**
  * Middleware to require guests to log in if there are no structure items
@@ -19,7 +22,14 @@ class MaybeRequireLogin
 
     public function handle(Request $request, Closure $next)
     {
-        if (Auth::guest() && !$this->permissions->guest()->isNotEmpty()) {
+        if (
+            Auth::guest() &&
+            !$this->permissions
+                ->guest()
+                ->whereIn('scope_type', [Channel::class, Page::class, StructureLink::class])
+                ->ability('view')
+                ->isNotEmpty()
+        ) {
             return redirect()->route('waterhole.login');
         }
 
