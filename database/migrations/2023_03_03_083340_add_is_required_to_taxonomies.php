caf5aa9d7637c606be56b34a3566ebb0f19daeb8
Toby Zerner <toby.zerner@gmail.com>
2023-03-03 09:05:29 +1030

    feat: option to make a taxonomy required

---
+++ database/migrations/2023_03_03_083340_add_is_required_to_taxonomies.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up()
+    {
+        Schema::table('taxonomies', function (Blueprint $table) {
+            $table->boolean('is_required')->default(0);
+        });
+    }
+
+    public function down()
+    {
+        Schema::table('taxonomies', function (Blueprint $table) {
+            $table->dropColumn('is_required');
+        });
+    }
+};
