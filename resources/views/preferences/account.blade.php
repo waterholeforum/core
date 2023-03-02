64792f70bdd5fdcae8e53f893854577cd8a0a983
Toby Zerner <toby.zerner@gmail.com>
2023-03-03 08:06:47 +1030

    feat: component list for account preferences

--- resources/views/preferences/account.blade.php
+++ resources/views/preferences/account.blade.php
@@ -3,74 +3,84 @@
     :title="__('waterhole::user.account-settings-title')"
 >
     <div class="card card__body stack dividers">
-        <div class="field">
-            <h4 class="field__label">
-                {{ __('waterhole::auth.name-label') }}
-            </h4>
-            <div>
-                {{ Auth::user()->name }}
+        @section('name')
+            <div class="field">
+                <h4 class="field__label">
+                    {{ __('waterhole::auth.name-label') }}
+                </h4>
+                <div>
+                    {{ Auth::user()->name }}
+                </div>
             </div>
-        </div>
+        @endsection
 
-        <div class="field">
-            <h4 class="field__label">
-                {{ __('waterhole::auth.email-label') }}
-            </h4>
-            <turbo-frame id="change-email">
-                <form
-                    action="{{ route('waterhole.preferences.email') }}"
-                    method="POST"
-                >
-                    @csrf
-                    <x-waterhole::field name="email">
-                        <div class="row gap-xs">
-                            <input
-                                class="grow"
-                                name="email"
-                                type="email"
-                                value="{{ old('email', Auth::user()->email) }}"
-                            >
-                            <button class="btn">
-                                {{ __('waterhole::system.change-button') }}
-                            </button>
-                        </div>
-                    </x-waterhole::field>
-                </form>
-            </turbo-frame>
-        </div>
+        @section('email')
+            <div class="field">
+                <h4 class="field__label">
+                    {{ __('waterhole::auth.email-label') }}
+                </h4>
+                <turbo-frame id="change-email">
+                    <form
+                        action="{{ route('waterhole.preferences.email') }}"
+                        method="POST"
+                    >
+                        @csrf
+                        <x-waterhole::field name="email">
+                            <div class="row gap-xs">
+                                <input
+                                    class="grow"
+                                    name="email"
+                                    type="email"
+                                    value="{{ old('email', Auth::user()->email) }}"
+                                >
+                                <button class="btn">
+                                    {{ __('waterhole::system.change-button') }}
+                                </button>
+                            </div>
+                        </x-waterhole::field>
+                    </form>
+                </turbo-frame>
+            </div>
+        @endsection
 
-        <div class="field">
-            <h4 class="field__label">
-                {{ __('waterhole::auth.password-label') }}
-            </h4>
-            <turbo-frame id="change-password">
-                <form
-                    action="{{ route('waterhole.preferences.password') }}"
-                    method="POST"
-                >
-                    @csrf
-                    <x-waterhole::field name="password">
-                        <div class="row gap-xs">
-                            <input
-                                autocomplete="new-password"
-                                class="grow"
-                                name="password"
-                                placeholder="{{ __('waterhole::auth.new-password-label') }}"
-                                type="password"
-                            >
-                            <button class="btn">
-                                {{ __('waterhole::system.change-button') }}
-                            </button>
-                        </div>
-                    </x-waterhole::field>
-                </form>
-            </turbo-frame>
-        </div>
+        @section('password')
+            <div class="field">
+                <h4 class="field__label">
+                    {{ __('waterhole::auth.password-label') }}
+                </h4>
+                <turbo-frame id="change-password">
+                    <form
+                        action="{{ route('waterhole.preferences.password') }}"
+                        method="POST"
+                    >
+                        @csrf
+                        <x-waterhole::field name="password">
+                            <div class="row gap-xs">
+                                <input
+                                    autocomplete="new-password"
+                                    class="grow"
+                                    name="password"
+                                    placeholder="{{ __('waterhole::auth.new-password-label') }}"
+                                    type="password"
+                                >
+                                <button class="btn">
+                                    {{ __('waterhole::system.change-button') }}
+                                </button>
+                            </div>
+                        </x-waterhole::field>
+                    </form>
+                </turbo-frame>
+            </div>
+        @endsection
 
-        <x-waterhole::action-button
-            :for="Auth::user()"
-            :action="Waterhole\Actions\DeleteSelf::class"
-            class="btn bg-danger"
-        />
+        @section('delete')
+            <x-waterhole::action-button
+                :for="Auth::user()"
+                :action="Waterhole\Actions\DeleteSelf::class"
+                class="btn bg-danger"
+            />
+        @endsection
+
+        @components(Waterhole\Extend\PreferencesAccount::components())
     </div>
 </x-waterhole::user-profile>
