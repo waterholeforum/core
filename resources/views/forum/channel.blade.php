a3c85cf162db51efdb91b0a9b10e977f0f72fd41
Toby Zerner <toby.zerner@gmail.com>
2023-03-05 20:07:52 +1030

    feat: add Atom feed <link> tags

--- resources/views/forum/channel.blade.php
+++ resources/views/forum/channel.blade.php
@@ -1,4 +1,8 @@
 <x-waterhole::layout :title="$channel->name" :data-channel="$channel->slug">
+    <x-slot:head>
+        <link rel="alternate" type="application/atom+xml" href="{{ route('waterhole.atom.channel', compact('channel')) }}">
+    </x-slot:head>
+
     <x-waterhole::index :channel="$channel">
         <x-waterhole::post-feed
             :feed="$feed"
--- resources/views/forum/home.blade.php
+++ resources/views/forum/home.blade.php
@@ -1,4 +1,8 @@
 <x-waterhole::layout>
+    <x-slot:head>
+        <link rel="alternate" type="application/atom+xml" href="{{ route('waterhole.atom.posts') }}">
+    </x-slot:head>
+
     <x-waterhole::index>
         <x-waterhole::post-feed :feed="$feed"/>
     </x-waterhole::index>
