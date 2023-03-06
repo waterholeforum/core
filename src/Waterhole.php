aa4a7d31cd6e110f780591e3f44bac5d8416fab4
Toby Zerner <toby.zerner@gmail.com>
2023-03-07 07:46:55 +1030

    refactor: rearrange methods

--- src/Waterhole.php
+++ src/Waterhole.php
@@ -9,25 +9,6 @@ abstract class Waterhole
 {
     public const VERSION = '0.1.0-dev';
 
-    public static function hasPendingMigrations(): bool
-    {
-        $migrator = app('migrator');
-        $files = $migrator->getMigrationFiles(__DIR__ . '/../database/migrations');
-        $repository = $migrator->getRepository();
-
-        if (!$repository->repositoryExists()) {
-            return true;
-        }
-
-        $ran = $repository->getRan();
-
-        return Collection::make($files)
-            ->reject(function ($file) use ($ran) {
-                return in_array(str_replace('.php', '', basename($file)), $ran);
-            })
-            ->isNotEmpty();
-    }
-
     public static function isForumRoute(): bool
     {
         return str_starts_with(request()->path(), config('waterhole.forum.path'));
@@ -46,5 +27,24 @@ abstract class Waterhole
     public static function permissions(): PermissionCollection
     {
         return app('waterhole.permissions');
+    }
+
+    public static function hasPendingMigrations(): bool
+    {
+        $migrator = app('migrator');
+        $files = $migrator->getMigrationFiles(__DIR__ . '/../database/migrations');
+        $repository = $migrator->getRepository();
+
+        if (!$repository->repositoryExists()) {
+            return true;
+        }
+
+        $ran = $repository->getRan();
+
+        return Collection::make($files)
+            ->reject(function ($file) use ($ran) {
+                return in_array(str_replace('.php', '', basename($file)), $ran);
+            })
+            ->isNotEmpty();
     }
 }
