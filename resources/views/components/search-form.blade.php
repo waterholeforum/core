9d365fdf169a01b62ef462bcd3f707fc918b5b1e
Toby Zerner <toby.zerner@gmail.com>
2023-03-09 08:07:26 +1030

    feat: add search-form component

---
+++ resources/views/components/search-form.blade.php
@@ -0,0 +1,28 @@
+<form
+    action="{{ route('waterhole.search') }}"
+    {{ $attributes->class('input-container') }}
+>
+    <x-waterhole::icon
+        icon="tabler-search"
+        class="no-pointer color-muted"
+    />
+
+    <input
+        class="rounded-full"
+        type="text"
+        name="q"
+        placeholder="{{ __('waterhole::forum.search-placeholder') }}"
+        required
+        data-hotkey="/"
+    >
+
+    <div class="hide-if-invalid">
+        <button
+            type="submit"
+            class="btn btn--icon btn--transparent btn--sm color-accent"
+            aria-label="{{ __('waterhole::forum.search-button') }}"
+        >
+            <x-waterhole::icon icon="tabler-arrow-right"/>
+        </button>
+    </div>
+</form>
