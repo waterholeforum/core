fe0acc9c46d47873eeedf3c4be4533e33ed1e0fd
Toby Zerner <toby.zerner@gmail.com>
2023-03-09 08:07:26 +1030

    feat: add search-form component

--- resources/views/components/header-search.blade.php
+++ resources/views/components/header-search.blade.php
@@ -6,31 +6,4 @@
     <ui-tooltip>{{ __('waterhole::forum.search-button') }}</ui-tooltip>
 </a>
 
-<form
-    action="{{ route('waterhole.search') }}"
-    class="input-container header-search__form hide-sm-down"
->
-    <x-waterhole::icon
-        icon="tabler-search"
-        class="no-pointer color-muted"
-    />
-
-    <input
-        class="rounded-full"
-        type="text"
-        name="q"
-        placeholder="{{ __('waterhole::forum.search-placeholder') }}"
-        required
-        data-hotkey="/"
-    >
-
-    <div class="hide-if-invalid">
-        <button
-            type="submit"
-            class="btn btn--icon btn--transparent btn--sm color-accent"
-            aria-label="{{ __('waterhole::forum.search-button') }}"
-        >
-            <x-waterhole::icon icon="tabler-arrow-right"/>
-        </button>
-    </div>
-</form>
+<x-waterhole::search-form class="header-search__form hide-sm-down"/>
