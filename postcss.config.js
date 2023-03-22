cff4111c285fc204c8896ef6bbc7a95028d302f7
Toby Zerner <toby.zerner@gmail.com>
2023-03-23 09:37:15 +1030

    refactor: convert to PostCSS

---
+++ postcss.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    parser: 'postcss-scss', // for single-line comment support
+    plugins: [
+        require('@csstools/postcss-global-data')({
+            files: ['./resources/css/system/_breakpoints.css'],
+        }),
+        require('postcss-each'),
+        require('postcss-preset-env')({
+            stage: 2,
+            features: {
+                'nesting-rules': true, // Stage 1
+            },
+        }),
+    ],
+};
--- webpack.mix.js
+++ webpack.mix.js
@@ -1,13 +1,15 @@
 const mix = require('laravel-mix');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
-mix.ts('resources/js/index.ts', 'resources/dist/index.js')
-    .ts('resources/js/highlight.ts', 'resources/dist/highlight.js')
-    .ts('resources/js/emoji.ts', 'resources/dist/emoji.js')
-    .ts('resources/js/admin/index.ts', 'resources/dist/admin.js')
+mix.setPublicPath('resources/dist');
 
-    .less('resources/less/forum/app.less', 'resources/dist/index.css')
-    .less('resources/less/admin/app.less', 'resources/dist/admin.css');
+mix.ts('resources/js/index.ts', 'resources/dist');
+mix.ts('resources/js/highlight.ts', 'resources/dist');
+mix.ts('resources/js/emoji.ts', 'resources/dist');
+mix.ts('resources/js/admin/index.ts', 'resources/dist/admin.js');
+
+mix.css('resources/css/forum/app.css', 'resources/dist/index.css');
+mix.css('resources/css/admin/app.css', 'resources/dist/admin.css');
 
 if (process.env.ANALYZE) {
     mix.webpackConfig({
