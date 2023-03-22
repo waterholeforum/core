0073ccc0e6157604168ea0aae7a7b5af4ff55dc4
Toby Zerner <toby.zerner@gmail.com>
2023-03-23 10:28:43 +1030

    refactor(design): rename breakpoints.css

--- postcss.config.js
+++ postcss.config.js
@@ -2,7 +2,7 @@ module.exports = {
     parser: 'postcss-scss', // for single-line comment support
     plugins: [
         require('@csstools/postcss-global-data')({
-            files: ['./resources/css/system/_breakpoints.css'],
+            files: ['./resources/css/system/breakpoints.css'],
         }),
         require('postcss-each'),
         require('postcss-preset-env')({
