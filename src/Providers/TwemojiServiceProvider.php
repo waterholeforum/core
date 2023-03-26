f55a6eb88a697f80491a1eba37be42192ea2bd34
Toby Zerner <toby.zerner@gmail.com>
2023-03-27 08:06:25 +1030

    fix: fix Twemoji image flicker on Safari

--- src/Providers/TwemojiServiceProvider.php
+++ src/Providers/TwemojiServiceProvider.php
@@ -19,8 +19,20 @@ class TwemojiServiceProvider extends ServiceProvider
     {
         $attributes['class'] = 'twemoji ' . ($attributes['class'] ?? '');
 
+        $attributes = array_merge(
+            [
+                'width' => '',
+                'height' => '',
+                // Don't use loading="lazy" because Safari has a bug which will
+                // cause the emoji to flicker when navigating with Turbo.
+                // https://bugs.webkit.org/show_bug.cgi?id=248025
+                'loading' => '',
+            ],
+            $attributes,
+        );
+
         return Twemoji::text($text)
             ->base(config('waterhole.design.twemoji_base'))
-            ->toHtml(null, array_merge(['width' => '', 'height' => ''], $attributes));
+            ->toHtml(null, $attributes);
     }
 }
