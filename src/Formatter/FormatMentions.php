e98fa7ed2836a1b1c124f39d286b29878b18bee1
Toby Zerner <toby.zerner@gmail.com>
2023-04-05 19:19:37 +0930

    perf: keep reference to mentions relationship and bail early

--- src/Formatter/FormatMentions.php
+++ src/Formatter/FormatMentions.php
@@ -89,11 +89,13 @@ abstract class FormatMentions
      */
     public static function rendering(Renderer $renderer, string &$xml, ?Context $context): void
     {
-        $xml = Utils::replaceAttributes($xml, 'MENTION', function ($attributes) use ($context) {
-            if (isset($attributes['id']) && $context?->model) {
-                $attributes['name'] = username(
-                    $user = $context->model->mentions->find($attributes['id']),
-                );
+        if (!($mentions = $context->model?->mentions)) {
+            return;
+        }
+
+        $xml = Utils::replaceAttributes($xml, 'MENTION', function ($attributes) use ($mentions) {
+            if (isset($attributes['id'])) {
+                $attributes['name'] = username($user = $mentions->find($attributes['id']));
 
                 if (!$user) {
                     unset($attributes['id']);
