46e5c44cd17d0f9acdc1c87ababf1f9f2fa34638
Toby Zerner <toby.zerner@gmail.com>
2023-04-05 16:57:11 +0930

    perf: add database indices

---
+++ database/migrations/2023_04_05_165546_add_post_id_created_at_index_to_comments.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->index(['post_id', 'created_at']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('comments', function (Blueprint $table) {
+            $table->dropIndex(['post_id', 'created_at']);
+        });
+    }
+};
---
+++ database/migrations/2023_04_05_165601_add_is_listed_position_index_to_structure.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('structure', function (Blueprint $table) {
+            $table->index(['is_listed', 'position']);
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('structure', function (Blueprint $table) {
+            $table->dropIndex(['is_listed', 'position']);
+        });
+    }
+};
