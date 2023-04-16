0e3a3e7bbdf050483a4e2ed8219e399ec855ef3b
Toby Zerner <toby.zerner@gmail.com>
2023-04-16 11:04:01 +0930

    chore: add missing migrations

---
+++ database/migrations/2023_04_13_163452_rename_channels_sandbox_to_ignore.php
@@ -0,0 +1,21 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->renameColumn('sandbox', 'ignore');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->renameColumn('ignore', 'sandbox');
+        });
+    }
+};
---
+++ database/migrations/2023_04_13_203835_add_layout_config_to_channels.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table
+                ->json('layout_config')
+                ->nullable()
+                ->after('layout');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::table('channels', function (Blueprint $table) {
+            $table->dropColumn('layout_config');
+        });
+    }
+};
