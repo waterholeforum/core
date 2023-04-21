ec8c5f7f1bbaf6685f577df0bc272666be8fc63e
Toby Zerner <toby.zerner@gmail.com>
2023-04-21 11:56:11 +0930

    feat: extract <x-waterhole::html> component

---
+++ src/View/Components/Html.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Waterhole\View\Components;
+
+use Illuminate\Support\Facades\Auth;
+use Illuminate\View\Component;
+
+class Html extends Component
+{
+    public array $payload;
+
+    public function __construct(
+        public ?string $title = null,
+        public ?string $titleSuffix = null,
+        public array $assets = [],
+    ) {
+        $this->titleSuffix ??= config('waterhole.forum.name');
+
+        $this->payload = [
+            'userId' => Auth::id(),
+            'debug' => config('app.debug'),
+            'echoConfig' => config('waterhole.system.echo_config'),
+            'twemojiBase' => config('waterhole.design.twemoji_base'),
+        ];
+    }
+
+    public function render()
+    {
+        return $this->view('waterhole::components.html');
+    }
+}
