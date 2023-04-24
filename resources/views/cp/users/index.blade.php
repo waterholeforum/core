9d6e4a5a95982a91112c280a70a0c075b9b32722
Toby Zerner <toby.zerner@gmail.com>
2023-04-24 19:44:05 +0930

    Fix users table width on mobile

--- resources/views/cp/users/index.blade.php
+++ resources/views/cp/users/index.blade.php
@@ -83,79 +83,71 @@
 
         <turbo-frame id="users_frame" target="_top" class="stack gap-md">
             @if ($users->isNotEmpty())
-                <div class="table-container card full-width" tabindex="0">
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                {{--                                <td class="choice-cell">--}}
-                                {{--                                    <label>--}}
-                                {{--                                        <input type="checkbox">--}}
-                                {{--                                    </label>--}}
-                                {{--                                </td>--}}
-                                @foreach (['name', 'email', 'groups', 'created_at', 'last_seen_at'] as $column)
-                                    <th>
-                                        @if (in_array($column, $sortable))
-                                            <a
-                                                href="{{ request()->fullUrlWithQuery(['sort' => $column, 'direction' => $sort === $column ? ($direction === 'asc' ? 'desc' : 'asc') : null]) }}"
-                                                class="with-icon color-text"
-                                            >
-                                                <span>
-                                                    {{ __('waterhole::cp.users-'.str_replace('_', '-', $column).'-column') }}
-                                                </span>
-                                                @if ($sort === $column)
-                                                    @icon('tabler-chevron-'.($direction === 'asc' ? 'up' : 'down'))
-                                                @endif
-                                            </a>
-                                        @else
-                                            {{ __('waterhole::cp.users-'.str_replace('_', '-', $column).'-column') }}
-                                        @endif
-                                    </th>
-                                @endforeach
-                                <th style="width: 1px"></th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            @foreach ($users as $user)
+                <div class="card">
+                    <div class="table-container full-width" tabindex="0">
+                        <table class="table">
+                            <thead>
                                 <tr>
-                                    {{--                                    <td class="choice-cell">--}}
-                                    {{--                                        <label>--}}
-                                    {{--                                            <input type="checkbox">--}}
-                                    {{--                                        </label>--}}
-                                    {{--                                    </td>--}}
-                                    <td>
-                                        <x-waterhole::user-label
-                                            :user="$user"
-                                            class="color-text"
-                                            link
-                                            target="_blank"
-                                        />
-                                    </td>
-                                    <td>
-                                        <a href="mailto:{{ $user->email }}">{{ Str::limit($user->email, 20) }}</a>
-                                    </td>
-                                    <td>
-                                        <x-waterhole::user-groups :user="$user"/>
-                                    </td>
-                                    <td>
-                                        <x-waterhole::time-ago :datetime="$user->created_at"/>
-                                    </td>
-                                    <td>
-                                        <x-waterhole::time-ago :datetime="$user->last_seen_at"/>
-                                    </td>
-                                    <td>
-                                        <x-waterhole::action-buttons
-                                            class="row text-xs"
-                                            :for="$user"
-                                            :button-attributes="['class' => 'btn btn--transparent btn--icon']"
-                                            tooltips
-                                            :limit="2"
-                                            placement="bottom-end"
-                                        />
-                                    </td>
+                                    @foreach (['name', 'email', 'groups', 'created_at', 'last_seen_at'] as $column)
+                                        <th>
+                                            @if (in_array($column, $sortable))
+                                                <a
+                                                    href="{{ request()->fullUrlWithQuery(['sort' => $column, 'direction' => $sort === $column ? ($direction === 'asc' ? 'desc' : 'asc') : null]) }}"
+                                                    class="with-icon color-text"
+                                                >
+                                                    <span>
+                                                        {{ __('waterhole::cp.users-'.str_replace('_', '-', $column).'-column') }}
+                                                    </span>
+                                                    @if ($sort === $column)
+                                                        @icon('tabler-chevron-'.($direction === 'asc' ? 'up' : 'down'))
+                                                    @endif
+                                                </a>
+                                            @else
+                                                {{ __('waterhole::cp.users-'.str_replace('_', '-', $column).'-column') }}
+                                            @endif
+                                        </th>
+                                    @endforeach
+                                    <th style="width: 1px"></th>
                                 </tr>
-                            @endforeach
-                        </tbody>
-                    </table>
+                            </thead>
+                            <tbody>
+                                @foreach ($users as $user)
+                                    <tr>
+                                        <td>
+                                            <x-waterhole::user-label
+                                                :user="$user"
+                                                class="color-text"
+                                                link
+                                                target="_blank"
+                                            />
+                                        </td>
+                                        <td>
+                                            <a href="mailto:{{ $user->email }}">{{ Str::limit($user->email, 20) }}</a>
+                                        </td>
+                                        <td>
+                                            <x-waterhole::user-groups :user="$user"/>
+                                        </td>
+                                        <td>
+                                            <x-waterhole::time-ago :datetime="$user->created_at"/>
+                                        </td>
+                                        <td>
+                                            <x-waterhole::time-ago :datetime="$user->last_seen_at"/>
+                                        </td>
+                                        <td>
+                                            <x-waterhole::action-buttons
+                                                class="row text-xs"
+                                                :for="$user"
+                                                :button-attributes="['class' => 'btn btn--transparent btn--icon']"
+                                                tooltips
+                                                :limit="2"
+                                                placement="bottom-end"
+                                            />
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
 
                 <div>
