cd5f1b9bb715309c04ee63498b3288a2652f6324
Qiaeru <22643035+qiaeru@users.noreply.github.com>
2023-05-25 11:28:41 +0200

    Translating "passwords.php" into French

--- lang/fr/passwords.php
+++ lang/fr/passwords.php
@@ -3,9 +3,9 @@
 // https://github.com/laravel/framework/blob/master/src/Illuminate/Translation/lang/en/passwords.php
 
 return [
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
-    'throttled' => 'Please wait before retrying.',
-    'token' => 'This password reset token is invalid.',
-    'user' => "We can't find a user with that email address.",
+    'reset' => 'Votre mot de passe a été réinitialisé !',
+    'sent' => 'Nous vous avons envoyé le lien de réinitialisation de votre mot de passe par courriel !',
+    'throttled' => 'Veuillez patienter avant de réessayer.',
+    'token' => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
+    'user' => "Nous ne trouvons pas d'utilisateur associé à cette adresse de courriel.",
 ];
