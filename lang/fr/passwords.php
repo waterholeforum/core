720eb7c4fe2d4fb612c7e4047a85faa58443ec39
tobyzerner <tobyzerner@users.noreply.github.com>
2023-05-25 11:08:14 +0000

    Run Prettier

--- lang/fr/passwords.php
+++ lang/fr/passwords.php
@@ -4,7 +4,8 @@
 
 return [
     'reset' => 'Votre mot de passe a été réinitialisé !',
-    'sent' => 'Nous vous avons envoyé le lien de réinitialisation de votre mot de passe par courriel !',
+    'sent' =>
+        'Nous vous avons envoyé le lien de réinitialisation de votre mot de passe par courriel !',
     'throttled' => 'Veuillez patienter avant de réessayer.',
     'token' => "Ce jeton de réinitialisation du mot de passe n'est pas valide.",
     'user' => "Nous ne trouvons pas d'utilisateur associé à cette adresse de courriel.",
