bda3b5b14e1d4ea5c92ca70aab36112cbd13d35c
Subarist <105265831+efast1568@users.noreply.github.com>
2023-06-15 20:23:17 +0800

    Add files via upload

---
+++ lang/passwords.php
@@ -0,0 +1,11 @@
+<?php
+
+// https://github.com/laravel/framework/blob/master/src/Illuminate/Translation/lang/en/passwords.php
+
+return [
+'reset' => '您的密碼已重設！',
+'sent' => '我們已將密碼重設連結發送到您的郵箱！',
+'throttled' => '請稍候再試。',
+'token' => '此密碼重設令牌無效。',
+'user' => '我們找不到使用該電子郵件地址的使用者。',
+];
