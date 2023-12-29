ebdc25bab07f1395e496a67a2f5b4839acc2cdfc
Toby Zerner <toby.zerner@gmail.com>
2023-12-29 10:40:38 +1030

    Add Authorizable contract to User

--- src/Models/User.php
+++ src/Models/User.php
@@ -5,6 +5,7 @@ namespace Waterhole\Models;
 use Illuminate\Auth\Authenticatable;
 use Illuminate\Auth\MustVerifyEmail;
 use Illuminate\Auth\Passwords\CanResetPassword;
+use Illuminate\Contracts\Auth\Access\Authorizable as AuthorizableContract;
 use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
 use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
 use Illuminate\Contracts\Auth\MustVerifyEmail as MustVerifyEmailContract;
@@ -58,6 +59,7 @@ use Waterhole\Notifications\VerifyEmail;
  */
 class User extends Model implements
     AuthenticatableContract,
+    AuthorizableContract,
     MustVerifyEmailContract,
     CanResetPasswordContract,
     HasLocalePreference
