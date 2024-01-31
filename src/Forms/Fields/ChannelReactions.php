151e6cca4c2e82decf70e67dd84dbb68e372e34f
Toby Zerner <toby.zerner@gmail.com>
2024-01-31 10:55:58 +1030

    Fix using incorrect database connection in some cases

--- src/Forms/Fields/ChannelReactions.php
+++ src/Forms/Fields/ChannelReactions.php
@@ -3,9 +3,11 @@
 namespace Waterhole\Forms\Fields;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rules\Exists;
 use Illuminate\Validation\Validator;
 use Waterhole\Forms\Field;
 use Waterhole\Models\Channel;
+use Waterhole\Models\ReactionSet;
 
 class ChannelReactions extends Field
 {
@@ -58,8 +60,8 @@ class ChannelReactions extends Field
     public function validating(Validator $validator): void
     {
         $validator->addRules([
-            'posts_reaction_set_id' => ['nullable', 'exists:reaction_sets,id'],
-            'comments_reaction_set_id' => ['nullable', 'exists:reaction_sets,id'],
+            'posts_reaction_set_id' => ['nullable', new Exists(ReactionSet::class, 'id')],
+            'comments_reaction_set_id' => ['nullable', new Exists(ReactionSet::class, 'id')],
         ]);
     }
 
