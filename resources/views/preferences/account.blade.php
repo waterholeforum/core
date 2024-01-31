fa01d20449d4e601e3b1bafafc5c84cfc687fade
Toby Zerner <toby.zerner@gmail.com>
2024-01-31 10:52:58 +1030

    Fix success message not appearing after change email/password

--- resources/views/preferences/account.blade.php
+++ resources/views/preferences/account.blade.php
@@ -23,24 +23,22 @@
                     <h4 class="field__label">
                         {{ __('waterhole::auth.email-label') }}
                     </h4>
-                    <turbo-frame id="change-email">
-                        <form action="{{ route('waterhole.preferences.email') }}" method="POST">
-                            @csrf
-                            <x-waterhole::field name="email">
-                                <div class="row gap-xs">
-                                    <input
-                                        class="grow"
-                                        name="email"
-                                        type="email"
-                                        value="{{ old('email', Auth::user()->email) }}"
-                                    />
-                                    <button class="btn">
-                                        {{ __('waterhole::system.change-button') }}
-                                    </button>
-                                </div>
-                            </x-waterhole::field>
-                        </form>
-                    </turbo-frame>
+                    <form action="{{ route('waterhole.preferences.email') }}" method="POST">
+                        @csrf
+                        <x-waterhole::field name="email">
+                            <div class="row gap-xs">
+                                <input
+                                    class="grow"
+                                    name="email"
+                                    type="email"
+                                    value="{{ old('email', Auth::user()->email) }}"
+                                />
+                                <button class="btn">
+                                    {{ __('waterhole::system.change-button') }}
+                                </button>
+                            </div>
+                        </x-waterhole::field>
+                    </form>
                 </div>
             @endif
         @endsection
@@ -51,25 +49,23 @@
                     <h4 class="field__label">
                         {{ __('waterhole::auth.password-label') }}
                     </h4>
-                    <turbo-frame id="change-password">
-                        <form action="{{ route('waterhole.preferences.password') }}" method="POST">
-                            @csrf
-                            <x-waterhole::field name="password">
-                                <div class="row gap-xs">
-                                    <input
-                                        autocomplete="new-password"
-                                        class="grow"
-                                        name="password"
-                                        placeholder="{{ __('waterhole::auth.new-password-label') }}"
-                                        type="password"
-                                    />
-                                    <button class="btn">
-                                        {{ __('waterhole::system.change-button') }}
-                                    </button>
-                                </div>
-                            </x-waterhole::field>
-                        </form>
-                    </turbo-frame>
+                    <form action="{{ route('waterhole.preferences.password') }}" method="POST">
+                        @csrf
+                        <x-waterhole::field name="password">
+                            <div class="row gap-xs">
+                                <input
+                                    autocomplete="new-password"
+                                    class="grow"
+                                    name="password"
+                                    placeholder="{{ __('waterhole::auth.new-password-label') }}"
+                                    type="password"
+                                />
+                                <button class="btn">
+                                    {{ __('waterhole::system.change-button') }}
+                                </button>
+                            </div>
+                        </x-waterhole::field>
+                    </form>
                 </div>
             @endif
         @endsection
