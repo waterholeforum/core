4e574bcec0ac4eebafa388211fb369d709b5c5bd
Daniel Sturm <384815+dsturm@users.noreply.github.com>
2024-02-01 12:36:45 +0100

    Fallback to ressource directory for translation bundle

--- src/Translation/FluentTranslator.php
+++ src/Translation/FluentTranslator.php
@@ -181,6 +181,10 @@ final class FluentTranslator implements TranslatorContract
 
                 return $bundle;
             }
+
+            // If there is no bundle in the namespace for the locale, try to load it from the
+            // ressource directory.
+            return $this->loadPath("{$this->path}/vendor/{$namespace}/", $locale, $group);
         }
 
         return false;
