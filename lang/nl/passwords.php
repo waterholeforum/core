6f65a12c2d5eca7bce93f5706dca8903ef74d461
tobyzerner <tobyzerner@users.noreply.github.com>
2024-03-17 09:32:28 +0000

    Run Prettier

--- lang/nl/passwords.php
+++ lang/nl/passwords.php
@@ -7,5 +7,5 @@ return [
     'sent' => 'We hebben je een link gestuurd om je wachtwoord te resetten!',
     'throttled' => 'Wacht even voordat je het opnieuw probeert.',
     'token' => 'Dit token voor wachtwoordreset is ongeldig.',
-    'user' => "We kunnen geen gebruiker vinden met dat e-mailadres.",
+    'user' => 'We kunnen geen gebruiker vinden met dat e-mailadres.',
 ];
