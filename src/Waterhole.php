2090181110698cf3dadb790c47581aa4d9efe8e8
BasilLangevin <basil.langevin@gmail.com>
2025-12-18 11:55:33 -0800

    Add missing domain check to `isForumRoute` and `isCpRoute`
    Currently, the `Waterhole::isForumRoute()` and `Waterhole::isCpRoute()` methods only check that the request path matches.
    
    This creates issues when a custom domain is specified in `config/system.php`, especially when integrated into an existing Laravel project.
    
    This commit adds a check to ensure the domain matches if one is specified in the `system` config.

--- src/Waterhole.php
+++ src/Waterhole.php
@@ -11,13 +11,36 @@ abstract class Waterhole
 {
     public const VERSION = '0.4.12';
 
+    public static function isWaterholeDomain(): bool
+    {
+        $domain = Config::get('waterhole.system.domain');
+
+        if (empty($domain)) {
+            return true;
+        }
+
+        if (str_contains($domain, '//')) {
+            $domain = parse_url($domain, PHP_URL_HOST);
+        }
+
+        return parse_url(Request::fullUrl(), PHP_URL_HOST) === $domain;
+    }
+
     public static function isForumRoute(): bool
     {
+        if (! static::isWaterholeDomain()) {
+            return false;
+        }
+
         return str_starts_with(Request::path(), Config::get('waterhole.forum.path'));
     }
 
     public static function isCpRoute(): bool
     {
+        if (! static::isWaterholeDomain()) {
+            return false;
+        }
+
         return str_starts_with(Request::path(), Config::get('waterhole.cp.path'));
     }
 
