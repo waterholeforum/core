2035eb21ee0121ae63bf020a9d5e175239b66a94
Toby Zerner <toby.zerner@gmail.com>
2026-01-04 18:17:53 +1030

    Update Laravel translations

--- lang/en/passwords.php
+++ lang/en/passwords.php
@@ -3,8 +3,8 @@
 // https://github.com/laravel/framework/blob/master/src/Illuminate/Translation/lang/en/passwords.php
 
 return [
-    'reset' => 'Your password has been reset!',
-    'sent' => 'We have emailed your password reset link!',
+    'reset' => 'Your password has been reset.',
+    'sent' => 'We have emailed your password reset link.',
     'throttled' => 'Please wait before retrying.',
     'token' => 'This password reset token is invalid.',
     'user' => "We can't find a user with that email address.",
