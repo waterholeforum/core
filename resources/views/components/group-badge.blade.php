edcbde49471087a342483545b9b767fcdc803b63
Toby Zerner <toby.zerner@gmail.com>
2026-01-25 18:34:47 +1030

    Stop Prettier bug repeatedly indenting this

--- resources/views/components/group-badge.blade.php
+++ resources/views/components/group-badge.blade.php
@@ -1,10 +1,7 @@
 <span
     {{ $attributes->class(["badge group-badge", "badge--hidden" => ! $group->is_public]) }}
     @if ($group->is_public && $group->color)
-        style="
-                                    --group-color: #{{ $group->color }};
-                                    --group-color-constrast: {{ Waterhole\get_contrast_color($group->color) }}
-                                "
+        style="--group-color: #{{ $group->color }}; --group-color-constrast: {{ Waterhole\get_contrast_color($group->color) }}"
     @endif
 >
     @if ($group->is_public && $group->icon)
