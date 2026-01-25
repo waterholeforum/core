a9f3b89063b5c3de27bd638aaeb93c93487e743d
tobyzerner <tobyzerner@users.noreply.github.com>
2026-01-25 08:03:33 +0000

    Run Prettier

--- resources/views/components/group-badge.blade.php
+++ resources/views/components/group-badge.blade.php
@@ -2,9 +2,9 @@
     {{ $attributes->class(["badge group-badge", "badge--hidden" => ! $group->is_public]) }}
     @if ($group->is_public && $group->color)
         style="
-                                --group-color: #{{ $group->color }};
-                                --group-color-constrast: {{ Waterhole\get_contrast_color($group->color) }}
-                            "
+                                    --group-color: #{{ $group->color }};
+                                    --group-color-constrast: {{ Waterhole\get_contrast_color($group->color) }}
+                                "
     @endif
 >
     @if ($group->is_public && $group->icon)
