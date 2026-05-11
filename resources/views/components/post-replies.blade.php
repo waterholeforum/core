b1e556e9f47dd58cee8a92eb8ff74eab2cc15a14
Toby Zerner <toby.zerner@gmail.com>
2026-05-11 10:25:34 +0930

    Simplify template

--- resources/views/components/post-replies.blade.php
+++ resources/views/components/post-replies.blade.php
@@ -1,25 +1,17 @@
 @blaze
-@props(['post'])
 
-@php
-    $count = $post->comment_count;
-    $labelKey = app()->getLocale() . ':' . $count;
-
-    static $labels = [];
-    $label = $labels[$labelKey] ??= [
-        'count' => Waterhole\compact_number($count),
-        'tooltip' => __('waterhole::forum.post-comments-link', ['count' => $count]),
-    ];
-@endphp
+@props([
+    'post',
+])
 
 <a
     href="{{ $post->url }}#comments"
-    class="btn btn--sm btn--outline @if (!$count) is-disabled @endif"
+    class="btn btn--sm btn--outline @if (!$post->comment_count) is-disabled @endif"
     data-action="post#appearAsRead"
 >
     @icon('tabler-message-circle-2')
-    <span>{{ $label['count'] }}</span>
+    <span>{{ Waterhole\compact_number($post->comment_count) }}</span>
     <ui-tooltip>
-        {{ $label['tooltip'] }}
+        {{ __('waterhole::forum.post-comments-link', ['count' => $post->comment_count]) }}
     </ui-tooltip>
 </a>
