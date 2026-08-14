723cd5200809596bf387b11b1738e24841e2096d
Toby Zerner <toby.zerner@gmail.com>
2026-08-14 13:50:54 +0930

    Truncate overflowing header titles

--- resources/views/components/header-title.blade.php
+++ resources/views/components/header-title.blade.php
@@ -1,6 +1,6 @@
 <a
     href="{{ route('waterhole.home') }}"
-    class="header-title nowrap h3"
+    class="header-title h3 shrink overflow-ellipsis"
     data-shortcut-trigger="navigation.home"
 >
     {{ config('waterhole.forum.name') }}
