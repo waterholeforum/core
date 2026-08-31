da0732ea49c6c102d3aaebce873caf28c69302a1
Toby Zerner <toby.zerner@gmail.com>
2026-08-31 17:16:31 +0930

    Constrain pinned posts to grid columns

--- resources/views/components/post-feed-pinned.blade.php
+++ resources/views/components/post-feed-pinned.blade.php
@@ -1,4 +1,4 @@
-<div class="post-feed__pinned grid-fit gap-md hide-if-empty">
+<div class="post-feed__pinned grid gap-md hide-if-empty">
     @foreach ($posts as $post)
         <x-waterhole::pinned-post :$post />
     @endforeach
